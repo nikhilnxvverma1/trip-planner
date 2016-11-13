8d79bc2a165679d780dca45afc9f0488b9b23e8f
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-11-13 04:30:24 -0500

    Integrated d3 js v3 and implemented a simple example bar chart

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -57164,6 +57164,100 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var TemperatureComponent = (function () {
     function TemperatureComponent() {
     }
+    TemperatureComponent.prototype.ngOnInit = function () {
+        this.initView();
+        return undefined;
+    };
+    TemperatureComponent.prototype.initView = function () {
+        var margin = { top: 20, right: 20, bottom: 70, left: 40 }, width = 600 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
+        // Parse the date / time
+        var parseDate = d3.time.format("%Y-%m").parse;
+        var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+        var y = d3.scale.linear().range([height, 0]);
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickFormat(d3.time.format("%Y-%m"));
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10);
+        var svg = d3.select("#temperatureSVG")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        d3.csv("bar-data.csv", function (error, data) {
+            data.forEach(function (d) {
+                d.date = parseDate(d.date);
+                d.value = +d.value;
+            });
+            x.domain(data.map(function (d) { return d.date; }));
+            y.domain([0, d3.max(data, function (d) { return d.value; })]);
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", "-.55em")
+                .attr("transform", "rotate(-90)");
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Value ($)");
+            svg.selectAll("bar")
+                .data(data)
+                .enter().append("rect")
+                .style("fill", "steelblue")
+                .attr("x", function (d) { return x(d.date); })
+                .attr("width", x.rangeBand())
+                .attr("y", function (d) { return y(d.value); })
+                .attr("height", function (d) { return height - y(d.value); });
+        });
+    };
+    TemperatureComponent.prototype.initViewD4 = function () {
+        var svg = d3.select("#temperatureSVG"), margin = { top: 20, right: 20, bottom: 30, left: 40 }, width = +svg.attr("width") - margin.left - margin.right, height = +svg.attr("height") - margin.top - margin.bottom;
+        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1), y = d3.scaleLinear().rangeRound([height, 0]);
+        var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        d3.tsv("data.tsv", function (d) {
+            d.frequency = +d.frequency;
+            return d;
+        }, function (error, data) {
+            if (error)
+                throw error;
+            x.domain(data.map(function (d) { return d.letter; }));
+            y.domain([0, d3.max(data, function (d) { return d.frequency; })]);
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y).ticks(10, "%"))
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .attr("text-anchor", "end")
+                .text("Frequency");
+            g.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function (d) { return x(d.letter); })
+                .attr("y", function (d) { return y(d.frequency); })
+                .attr("width", x.bandwidth())
+                .attr("height", function (d) { return height - y(d.frequency); });
+        });
+    };
     TemperatureComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["F" /* Component */])({
             selector: 'temperature',
@@ -60538,7 +60632,7 @@ module.exports = "<div class=\"card small saved-item\">\n    <div class=\"card-i
 /* 669 */
 /***/ function(module, exports) {
 
-module.exports = "<h1>temprature</h1>"
+module.exports = "<h1>temprature</h1>\n<svg id=\"temperatureSVG\" width=\"960\" height=\"500\"></svg>"
 
 /***/ },
 /* 670 */
