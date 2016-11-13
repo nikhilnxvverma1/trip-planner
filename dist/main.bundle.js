6eefd3e8b3874870052daa6f0e0e8d94210a8c3c
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-11-13 05:10:58 -0500

    Integrated datamaps library for location with an example

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -57071,6 +57071,71 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var LocationComponent = (function () {
     function LocationComponent() {
     }
+    LocationComponent.prototype.ngOnInit = function () {
+        this.updateView();
+        return undefined;
+    };
+    LocationComponent.prototype.updateView = function () {
+        var bubble_map = new Datamap({
+            element: document.getElementById("location-map"),
+            geographyConfig: {
+                popupOnHover: false,
+                highlightOnHover: false
+            },
+            fills: {
+                defaultFill: '#ABDDA4',
+                USA: 'blue',
+                RUS: 'red'
+            }
+        });
+        bubble_map.bubbles([
+            {
+                name: 'Not a bomb, but centered on Brazil',
+                radius: 23,
+                centered: 'BRA',
+                country: 'USA',
+                yeild: 0,
+                fillKey: 'USA',
+                date: '1954-03-01'
+            },
+            {
+                name: 'Not a bomb',
+                radius: 15,
+                yeild: 0,
+                country: 'USA',
+                centered: 'USA',
+                date: '1986-06-05',
+                significance: 'Centered on US',
+                fillKey: 'USA'
+            },
+            {
+                name: 'Castle Bravo',
+                radius: 25,
+                yeild: 15000,
+                country: 'USA',
+                significance: 'First dry fusion fuel "staged" thermonuclear weapon; a serious nuclear fallout accident occurred',
+                fillKey: 'USA',
+                date: '1954-03-01',
+                latitude: 11.415,
+                longitude: 165.1619
+            }, {
+                name: 'Tsar Bomba',
+                radius: 70,
+                yeild: 50000,
+                country: 'USSR',
+                fillKey: 'RUS',
+                significance: 'Largest thermonuclear weapon ever tested—scaled down from its initial 100 Mt design by 50%',
+                date: '1961-10-31',
+                latitude: 73.482,
+                longitude: 54.5854
+            }
+        ], {
+            popupTemplate: function (geo, data) {
+                return '<div class="hoverinfo">Yield:' + data.yeild +
+                    'Exploded on ' + data.date + ' by the ' + data.country + '';
+            }
+        });
+    };
     LocationComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["F" /* Component */])({
             selector: 'location',
@@ -60614,7 +60679,7 @@ module.exports = "<h1>distance</h1>"
 /* 666 */
 /***/ function(module, exports) {
 
-module.exports = "<h1>location</h1>"
+module.exports = "<h1>location</h1>\n<div id=\"location-map\" class=\"map\"></div>"
 
 /***/ },
 /* 667 */
